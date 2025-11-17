6f19ea75e552cc6d858cbf70eb062df1ca836643
PSHEMA <p.shema@alustudent.com>
2025-11-17 19:24:27 +0200

    Ag

--- agrisync-backend/agrisync/settings.py
+++ agrisync-backend/agrisync/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d$3hitcpm48gw7q%zc54=!fcp4a^q+)ju!q&-^+c@e&0mb8vek'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -142,6 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
